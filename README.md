017e0e80787b01d6cd96c4b72c0d60b4de16b42b
alexshelto <alexandershelton@icloud.com>
2020-04-22 14:52:29 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# chip8-emulation
-Building an emulator for .ch8 (Chip-8) programs
+#
+
+
+
+
+
+
+
+
+
+
+## Instruction Set
+https://en.wikipedia.org/wiki/CHIP-8
+
+
+### Register Placeholders
+'x' & 'y' are used in place of register identifiers.
+'x' & 'y' are replaced with one of 16 nums inside of 0-F
+
+### Immediate Values
+* 'N' is used where the number is interpreted as immediate.  (4-bit)
+* 'NN' is used for a byte of data (8-bit)
+* 'NNN' is used to signify an address
+
+### 0xxx Opcode
+There are 3 ops starting with 0... <br>
+the first returns from a sub routine, the seccond clears screen, and the third calls RCA 1802 (wont be neccesary)
+
+### 1xxx, 2xxx Bxxx Opcode
+* 1xxx are absolute jumps, the opcode: 14A6 equates to `JUMP $4A6`
+* 2NNN jumps to subroutine
+* 24A8 would equate to: `CALL $4AB`
+    * pushed the address if the next instruction is on the stack
+    * setting the PC 'Program Counter' to the address contained in the instruction
+* Each subroutine must end with 'OOEE' instruction
+
+### 3xxx, 4xxx, 5xxx, 9xxx Opcode: Conditionals
+| Oppcode | Nemonic |
+|---------|---------|
+|3A00     | EQ  VA, #$00|
+|4800     | NE  V8, #$00|
+|5A70     | EQ  VA, V7  |
+|93B0     | NE  V3. VB  |
+where EQ checks for equality and NE checks for not equal
+
+### 9xxx, Register Opcode
+* There are 9 instructions in the 8xxx group that use 2 registers
+* Vx gets modified by Vy
+Some opperations will midify 'VF' based on the result of the math or storing numbers. this is chip 8's condition codes <br>
+For the assembly syntax, a dot '.' after instructions indicates the modification of 'VF'
+| Opcode | Nemonic |
+|--------|---------|    
+|83A0|     MOV   V3, VA|    
+|83A1|     OR    V3, VA|    
+|83A2|     AND   V3, VA|   
+|83A3|     XOR   V3, VA|   
+|83B4|     ADD.  V3, VB|    
+|83B5|     SUB.  V3, VB|    
+|83x6|     SHR.  V3    |
+|83B7|     SUBB. V3, VB|   
+|83xE|     SHL.  V3    |
+
+### The rest of Chip-8 Opcodes...
+* The rest of the Opcodes cannot be easilly classified. Most have to do with timers, sprites, and interactions
+* below is the syntax for them and you can read more in the wiki
+| Opcode | Nemonic |
+|--------|---------|    
+|DXYN|    SPRITE       VX, VY, #$N |   
+|EX9E|    SKIP.KEY     VX          |
+|EXA1|    SKIP.NOKEY   VX          |
+|FX07|    MOV          VX, DELAY   |
+|FX0A|    WAITKEY      VX          |
+|FX15|    MOV          DELAY, VX   | 
+|FX18|    MOV          SOUND, VX   | 
+|FX1E|    ADD          I, VX       |
+|FX29|    SPRITECHAR   VX          |
+|FX33|    MOVBCD       VX          |
+|FX55|    MOVM         (I), V0-VX  | 
+|FX65|    MOVM         V0-VX, (I)  |
+
---
+++ src/disassembler.c
@@ -0,0 +1,82 @@
+/**
+ * @file disassembler.c
+ * @brief 
+ * @date 2020-04-22
+ */
+
+#include "stdint.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+
+void DisassembleChip8(uint8_t *codeBuffer, int programCounter) {
+    uint8_t *code = &codeBuffer[programCounter];
+    uint8_t firstNib = code[0] >> 4;
+    printf("%04x %02x %02x ", programCounter, code[0], code[1]);    
+    switch (firstNib)    
+    {    
+        case 0x00: printf("0 not handled yet"); break;    
+        case 0x01: printf("1 not handled yet"); break;    
+        case 0x02: printf("2 not handled yet"); break;    
+        case 0x03: printf("3 not handled yet"); break;    
+        case 0x04: printf("4 not handled yet"); break;    
+        case 0x05: printf("5 not handled yet"); break;    
+        case 0x06:    
+            {    
+                uint8_t reg = code[0] & 0x0f;    
+                printf("%-10s V%01X,#$%02x", "MVI", reg, code[1]);    
+            }    
+            break;    
+        case 0x07: printf("7 not handled yet"); break;    
+        case 0x08: printf("8 not handled yet"); break;    
+        case 0x09: printf("9 not handled yet"); break;    
+        case 0x0a:    
+            {   
+                uint8_t addresshi = code[0] & 0x0f;    
+                printf("%-10s I,#$%01x%02x", "MVI", addresshi, code[1]);    
+            }    
+            break;    
+        case 0x0b: printf("b not handled yet"); break;    
+        case 0x0c: printf("c not handled yet"); break;    
+        case 0x0d: printf("d not handled yet"); break;    
+        case 0x0e: printf("e not handled yet"); break;    
+        case 0x0f: printf("f not handled yet"); break;    
+    }    
+}    
+
+   int main (int argc, char**argv)    
+   {    
+    FILE *f = fopen(argv[1], "rb");    
+    if (f==NULL)    
+    {    
+        printf("error: Couldn't open %s\n", argv[1]);    
+        EXIT_FAILURE;    
+    }
+
+    //Get the file size    
+    fseek(f, 0L, SEEK_END);    
+    int fsize = ftell(f);
+    fseek(f, 0L, SEEK_SET);    
+
+/* ---------------------------------- debug --------------------------------- */
+    printf("Size of file: %x \n", fsize);   
+    printf("Allocating a buffer of size: %x \n", fsize + 0x200);    
+
+    //CHIP-8 convention puts programs in memory at 0x200    
+    //Starting at 0x200
+    // They will all have hardcoded addresses expecting that    
+
+    //Read the file into memory at 0x200 and close it.    
+    unsigned char *buffer = malloc(fsize+0x200);
+    fread(buffer+0x200, fsize, 1, f); //
+    fclose(f);    
+
+    int pc = 0x200;    
+    while (pc < (fsize+0x200))    
+    {
+        DisassembleChip8(buffer, pc);    
+        pc += 2; //step 2 in hex eg: (228 += 2 => 22a)
+        printf ("\n");    
+    }
+    EXIT_SUCCESS;    
+}    
